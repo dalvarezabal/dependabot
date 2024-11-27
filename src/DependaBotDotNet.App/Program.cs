4ab09485ec1d1400d98dc062bab1ff15f5f72ed5
David Alvarez Abal <david.alvarez@soderbergpartners.se>
2024-11-27 11:34:29 +0100

    Initial Commit

---
+++ src/DependaBotDotNet.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DependaBotDotNet.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var dbOptions = new DbContextOptionsBuilder();
+        }
+    }
+}
